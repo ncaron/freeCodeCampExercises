a0eac06b1c96a73fac57728d052850fe6babfa73
Niko Caron <niko.caron90@gmail.com>
2020-04-11 02:52:58 +0200

    Decrement a Number with JavaScript

---
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/Decrement a Number with JavaScript/index.js
@@ -0,0 +1,6 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/decrement-a-number-with-javascript
+
+var myVar = 11;
+
+// Only change code below this line
+myVar--;
