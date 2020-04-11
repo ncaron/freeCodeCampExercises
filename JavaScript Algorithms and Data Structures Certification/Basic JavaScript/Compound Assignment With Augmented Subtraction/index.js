ff59e0557a087d6c2c5ca97e713fc106ad0bd772
Niko Caron <niko.caron90@gmail.com>
2020-04-11 15:25:35 +0200

    Compound Assignment With Augmented Subtraction

---
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/Compound Assignment With Augmented Subtraction/index.js
@@ -0,0 +1,10 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/compound-assignment-with-augmented-subtraction
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
