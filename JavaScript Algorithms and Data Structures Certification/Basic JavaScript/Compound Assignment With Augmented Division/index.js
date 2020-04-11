0f1c6de8ac0fd3f826085ed4d587426e4237d5c7
Niko Caron <niko.caron90@gmail.com>
2020-04-11 15:27:12 +0200

    Compound Assignment With Augmented Division

---
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/Compound Assignment With Augmented Division/index.js
@@ -0,0 +1,10 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/compound-assignment-with-augmented-division
+
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
