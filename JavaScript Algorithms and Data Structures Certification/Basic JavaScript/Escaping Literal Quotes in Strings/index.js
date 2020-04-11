20c6805d5d5121eaa29449ec6fa591b42edf198c
Niko Caron <niko.caron90@gmail.com>
2020-04-11 15:33:34 +0200

    Escaping Literal Quotes in Strings

---
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/Escaping Literal Quotes in Strings/index.js
@@ -0,0 +1,3 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/escaping-literal-quotes-in-strings
+
+const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
