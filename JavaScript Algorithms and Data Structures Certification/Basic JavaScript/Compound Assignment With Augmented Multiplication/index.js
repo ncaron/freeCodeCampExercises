9dcbfb3afe79efe07a3ded657337f516f6c748e1
Niko Caron <niko.caron90@gmail.com>
2020-04-11 15:26:18 +0200

    Compound Assignment With Augmented Multiplication

---
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/Compound Assignment With Augmented Multiplication/index.js
@@ -0,0 +1,10 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/compound-assignment-with-augmented-multiplication
+
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
