0de7fcdd037aa5e561c89cdc7d77be571855cb16
Niko Caron <niko.caron90@gmail.com>
2020-04-11 14:51:57 +0200

    Compound Assignment With Augmented Addition

---
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/Compound Assignment With Augmented Addition/index.js
@@ -0,0 +1,10 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/compound-assignment-with-augmented-addition
+
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
