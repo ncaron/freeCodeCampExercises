74ca9c93a1ce83178ab7df6e0f5eebdcc884137f
Niko Caron <niko.caron90@gmail.com>
2020-04-11 14:50:05 +0200

    Finding a Remainder in JavaScript

---
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/Finding a Remainder in JavaScript/index.js
@@ -0,0 +1,5 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/finding-a-remainder-in-javascript
+
+// Only change code below this line
+
+var remainder = 11 % 3;
