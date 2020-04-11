7b5f2390f98143967b89bea48caaf5baa849887d
Niko Caron <niko.caron90@gmail.com>
2020-04-11 14:41:51 +0200

    Create Decimal Numbers with JavaScript

---
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/Create Decimal Numbers with JavaScript/index.js
@@ -0,0 +1,7 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/create-decimal-numbers-with-javascript
+
+var ourDecimal = 5.7;
+
+// Only change code below this line
+
+const myDecimal = 4.2;
