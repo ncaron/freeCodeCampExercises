5d136fe112b023f746b28ce9be19326d414b8694
Niko Caron <niko.caron90@gmail.com>
2020-04-20 18:07:36 +0200

    Constructing Strings with Variables

---
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/Constructing Strings with Variables/index.js
@@ -0,0 +1,5 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/constructing-strings-with-variables
+
+// Only change code below this line
+const myName = 'Niko';
+const myStr = 'My name is ' + myName + ' and I am well!';
