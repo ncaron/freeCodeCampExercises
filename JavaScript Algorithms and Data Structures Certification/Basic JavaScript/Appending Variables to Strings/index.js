acc66613f2c6675a0ecbd207e7a006dcef7752df
Niko Caron <niko.caron90@gmail.com>
2020-04-21 17:36:20 +0200

    Appending Variables to Strings

---
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/Appending Variables to Strings/index.js
@@ -0,0 +1,7 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/appending-variables-to-strings
+
+// Change code below this line
+
+const someAdjective = 'fun';
+let myStr = 'Learning to code is ';
+myStr += someAdjective;
