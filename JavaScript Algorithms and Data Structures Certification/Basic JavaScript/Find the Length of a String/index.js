3ff3f9123221c46d39d844d22aecaacfa401f11c
Niko Caron <niko.caron90@gmail.com>
2020-04-22 20:16:25 +0200

    Find the Length of a String

---
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/Find the Length of a String/index.js
@@ -0,0 +1,9 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/find-the-length-of-a-string
+
+// Setup
+var lastNameLength = 0;
+var lastName = 'Lovelace';
+
+// Only change code below this line
+
+lastNameLength = lastName.length;
