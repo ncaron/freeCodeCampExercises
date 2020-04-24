c4de7195f3bceb1c7c2297d0683195a2bee02e4b
Niko Caron <niko.caron90@gmail.com>
2020-04-24 17:29:16 +0200

    Understand String Immutability

---
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/Understand String Immutability/index.js
@@ -0,0 +1,8 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/understand-string-immutability
+
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+myStr = 'Hello World'; // Change this line
+// Only change code above this line
