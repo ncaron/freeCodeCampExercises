1dfdfe0394f497f34346e8d03f55d9102123db74
Niko Caron <niko.caron90@gmail.com>
2020-04-25 16:51:25 +0200

    Nest one Array within Another Array

---
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/Nest one Array within Another Array/index.js
@@ -0,0 +1,4 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/nest-one-array-within-another-array
+
+// Only change code below this line
+var myArray = [[], []];
