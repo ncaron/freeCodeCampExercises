47d28d1db7ec48aa7ea0a40483de8f01fe960e5d
Niko Caron <niko.caron90@gmail.com>
2020-04-25 16:53:07 +0200

    Access Array Data with Indexes

---
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/Access Array Data with Indexes/index.js
@@ -0,0 +1,8 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/access-array-data-with-indexes
+
+// Setup
+var myArray = [50, 60, 70];
+
+// Only change code below this line
+
+const myData = myArray[0];
