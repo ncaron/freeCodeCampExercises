2c1f72cff1049628ea724073e503fa3a249d357f
Niko Caron <niko.caron90@gmail.com>
2020-04-25 17:01:49 +0200

    Write Reusable JavaScript with Functions

---
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/Write Reusable JavaScript with Functions/index.js
@@ -0,0 +1,7 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/write-reusable-javascript-with-functions
+
+function reusableFunction() {
+  console.log('Hi World');
+}
+
+reusableFunction();
