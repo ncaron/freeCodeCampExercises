9128f49b956e5752a5cfbc4e4f2ee74688806400
Niko Caron <niko.caron90@gmail.com>
2020-04-25 16:58:47 +0200

    Manipulate Arrays With unshift()

---
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/Manipulate Arrays With unshift()/index.js
@@ -0,0 +1,11 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/manipulate-arrays-with-unshift
+
+// Setup
+var myArray = [
+  ['John', 23],
+  ['dog', 3],
+];
+myArray.shift();
+
+// Only change code below this line
+myArray.unshift(['Paul', 35]);
