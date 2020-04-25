5ef7322db88642d7947f851e27385a1dae961e55
Niko Caron <niko.caron90@gmail.com>
2020-04-25 16:47:55 +0200

    Word Blanks

---
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/Word Blanks/index.js
@@ -0,0 +1,10 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/word-blanks
+
+var myNoun = "dog";
+var myAdjective = "big";
+var myVerb = "ran";
+var myAdverb = "quickly";
+
+// Only change code below this line
+var wordBlanks = 'The ' + myNoun + ' ' + myVerb + ' ' + myAdverb + ' over the ' + myAdjective + ' log.'; // Change this line
+// Only change code above this line
