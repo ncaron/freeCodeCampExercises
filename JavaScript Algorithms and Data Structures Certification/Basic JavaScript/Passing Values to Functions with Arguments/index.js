f7b156ac7b205def3edc925da30d6aaf4a65f4ba
Niko Caron <niko.caron90@gmail.com>
2020-04-25 17:04:37 +0200

    Passing Values to Functions with Arguments

---
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/Passing Values to Functions with Arguments/index.js
@@ -0,0 +1,7 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/passing-values-to-functions-with-arguments
+
+function functionWithArgs(num1, num2) {
+  console.log(num1 + num2);
+}
+
+functionWithArgs(1, 2);
