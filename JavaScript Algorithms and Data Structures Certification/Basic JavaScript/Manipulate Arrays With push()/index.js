67ed2617866782c06842e17a8afcec2a0b5b210e
Niko Caron <niko.caron90@gmail.com>
2020-04-25 16:56:01 +0200

    Manipulate Arrays With push()

---
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/Manipulate Arrays With push()/index.js
@@ -0,0 +1,10 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/manipulate-arrays-with-push
+
+// Setup
+var myArray = [
+  ['John', 23],
+  ['cat', 2],
+];
+
+// Only change code below this line
+myArray.push(['dog', 3]);
