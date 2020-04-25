507ae5b0e935595ca227dd5ad67f8b9b068ddf57
Niko Caron <niko.caron90@gmail.com>
2020-04-25 17:00:30 +0200

    Shopping List

---
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/Shopping List/index.js
@@ -0,0 +1,9 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/shopping-list
+
+const myList = [];
+
+myList.push(['Meat', 5]);
+myList.push(['Veggies', 2]);
+myList.push(['Fruits', 4]);
+myList.push(['Snacks', 1]);
+myList.push(['Bread', 3]);
