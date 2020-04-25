ba0eb4175d7a25154bce64ce3b6421257d59f3c0
Niko Caron <niko.caron90@gmail.com>
2020-04-25 16:50:45 +0200

    Store Multiple Values in one Variable using JavaScript Arrays

---
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/Store Multiple Values in one Variable using JavaScript Arrays/index.js
@@ -0,0 +1,4 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/store-multiple-values-in-one-variable-using-javascript-arrays
+
+// Only change code below this line
+var myArray = ['test', 42];
