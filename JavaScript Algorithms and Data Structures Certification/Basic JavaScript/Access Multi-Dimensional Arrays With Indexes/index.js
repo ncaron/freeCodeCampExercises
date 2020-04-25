3a5e13b03abf6a3d1fa4a642a55f20f4aea776cc
Niko Caron <niko.caron90@gmail.com>
2020-04-25 16:54:56 +0200

    Access Multi-Dimensional Arrays With Indexes

---
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/Access Multi-Dimensional Arrays With Indexes/index.js
@@ -0,0 +1,12 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/access-multi-dimensional-arrays-with-indexes
+
+// Setup
+var myArray = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [[10, 11, 12], 13, 14],
+];
+
+// Only change code below this line
+var myData = myArray[2][1];
