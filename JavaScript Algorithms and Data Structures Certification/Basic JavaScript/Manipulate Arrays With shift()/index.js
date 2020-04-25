81b5aa013a6599b0135ab123a8566a353fcd3523
Niko Caron <niko.caron90@gmail.com>
2020-04-25 16:57:48 +0200

    Manipulate Arrays With shift()

---
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/Manipulate Arrays With shift()/index.js
@@ -0,0 +1,10 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/manipulate-arrays-with-shift
+
+// Setup
+var myArray = [
+  ['John', 23],
+  ['dog', 3],
+];
+
+// Only change code below this line
+var removedFromMyArray = myArray.shift();
