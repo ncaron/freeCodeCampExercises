048748bb79ba725a51ad4187536400675767c6c4
Niko Caron <niko.caron90@gmail.com>
2020-04-25 16:53:58 +0200

    Modify Array Data With Indexes

---
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/Modify Array Data With Indexes/index.js
@@ -0,0 +1,7 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/modify-array-data-with-indexes
+
+// Setup
+var myArray = [18, 64, 99];
+
+// Only change code below this line
+myArray[0] = 45;
