be5d75a6b1ed09c00b8937e10c532bba16dba8f5
Niko Caron <niko.caron90@gmail.com>
2020-04-27 17:39:17 +0200

    Completed 5 exercises
    Return a Value from a Function with Return
    Understanding Undefined Value returned from a Function
    Assignment with a Returned Value
    Stand in Line
    Understanding Boolean Values

---
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/Return a Value from a Function with Return/index.js
@@ -0,0 +1,5 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/return-a-value-from-a-function-with-return
+
+function timesFive(num) {
+  return num * 5;
+}
